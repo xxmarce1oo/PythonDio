7abfa90cca1a4c412fd0fed200a3797e5a9cc992
Marcelo <marcelo2@ufpr.br>
2023-08-14 14:15:04 -0300

    novos

---
+++ Conceitos Básicos/associação.py
@@ -0,0 +1,8 @@
+curso = "Curso de python"
+frutas = ["Maçã", "banana", "uva"]
+saques = [1500,100]
+
+
+print("python" in curso)
+print("Laranaja" not in frutas)
+print(200 in saques)
---
+++ aguardando.py
@@ -0,0 +1,23 @@
+import os
+
+ponto = "."
+ponto2 = ".."
+ponto3 = "..."
+
+for i in range(0,100000000000000000):
+    os.system("cls")
+    
+    for j in ponto:
+        print("Aguardando"+ ponto)
+        os.system("cls")
+
+        
+        for k in ponto2:
+            print("Aguardando"+ponto2)
+            os.system("cls")
+
+             
+        for l in ponto3:
+            print("Aguardando"+ponto3)
+            os.system("cls")
+
