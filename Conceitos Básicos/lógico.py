a3951b7d93f7d595208173fea85890d4a3607343
Marcelo <marcelo2@ufpr.br>
2023-08-14 09:30:36 -0300

    novo

---
+++ Conceitos Básicos/lógico.py
@@ -0,0 +1,22 @@
+saldo = 1000
+saque = 200
+limite = 100
+
+print(saldo >= saque and saque<=limite) # and ambas as condições precisam ser verdadeiras
+
+print(saldo >= saque or  saque<=limite) # or apenas uma condição precisa ser verdadeira
+
+# negação
+
+print("negação")
+
+contatos_emergencia = []
+
+print(not 1000 > 1500)
+
+print(not contatos_emergencia)
+
+print(not "marcelo")
+
+print(not "")
+ 
