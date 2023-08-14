c95bac3624c55ce64ca5fc099179225e2c2bf03f
Marcelo <marcelo2@ufpr.br>
2023-08-14 14:30:29 -0300

    ops

--- aguardando.py
+++ aguardando.py
@@ -1,27 +1,11 @@
 import os
 import time
 
-ponto = "."
-ponto2 = ".."
-ponto3 = "..."
+pontos = [".", "..", "..."]
 
 while True:
-    os.system("cls")
 
-    for j in ponto:
+    for ponto in pontos:
         print("Aguardando" + ponto)
-        time.sleep(0.5)  
+        time.sleep(0.5)
         os.system("cls")
-
-
-    for k in ponto2:
-        print("Aguardando" + ponto2)
-        time.sleep(0.25)
-        os.system("cls")
-
-
-    for l in ponto3:
-        print("Aguardando" + ponto3)
-        time.sleep(0.125)
-        os.system("cls")
-
