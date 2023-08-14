a427db2062bba380fe5e207915f69de6c9c6da82
Marcelo <marcelo2@ufpr.br>
2023-08-14 09:57:57 -0300

    novo

---
+++ Conceitos Básicos/identação.py
@@ -0,0 +1,17 @@
+def sacar(valor):
+    
+    saldo = 500
+    
+    if saldo>=valor:
+        print("valor sacado")
+    print("Sair?")        
+
+
+def depositar(valor):
+    saldo = 500
+    saldo += valor
+
+
+sacar(500) 
+        
+        
